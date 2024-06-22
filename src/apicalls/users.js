f09b5934cc2040ea905e5b18da5fcd4adad4934b
Neil Quizon <neil.quizon@gmail.com>
2024-06-22 13:02:38 -0700

    hased password and check duplicate users

--- src/apicalls/users.js
+++ src/apicalls/users.js
@@ -1,11 +1,31 @@
 import { message } from "antd";
 import firestoreDatabase from "../firebaseConfig";
-import { collection, addDoc } from "firebase/firestore";
+import { collection, addDoc , getDocs , query , where } from "firebase/firestore";
+import CryptoJS from "crypto-js";
+
 
 
 export const CreateUser = async (payload) => {
 
     try {
+
+        // check if user already exist in the database using email
+
+        const qry = query(collection(firestoreDatabase, "users"), where("email", "==", payload.email));
+
+        const querySnapshot = await getDocs(qry);
+        if (querySnapshot.size > 0) {
+
+            throw new Error("user already existx");
+        }
+
+        // hash password
+        const hashedPassword = CryptoJS.AES.encrypt(
+            payload.password,
+            "findvancouverdoctor"
+        ).toString();
+        payload.password = hashedPassword;
+
 
         const docRef = collection(firestoreDatabase, "users");
         await addDoc(docRef, payload);
