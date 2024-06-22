a7d391dea3087c625b3588601cf2bc481220a56c
Neil Quizon <neil.quizon@gmail.com>
2024-06-22 13:37:08 -0700

    updated cryptojs

--- src/apicalls/users.js
+++ src/apicalls/users.js
@@ -1,6 +1,6 @@
 import { message } from "antd";
 import firestoreDatabase from "../firebaseConfig";
-import { collection, addDoc , getDocs , query , where } from "firebase/firestore";
+import { collection, addDoc, getDocs, query, where } from "firebase/firestore";
 import CryptoJS from "crypto-js";
 
 
@@ -38,3 +38,38 @@ export const CreateUser = async (payload) => {
         return error;
     }
 }
+
+export const LoginUser = async (payload) => {
+
+    try {
+
+        const qry = query(
+            collection(firestoreDatabase, "users"),
+            where("email", "==", payload.email)
+        );
+
+        const userSnapshots = await getDocs(qry);
+        if (userSnapshots.size === 0) {
+            throw new Error("User does not exist");
+        }
+
+        // decrypt password
+        const user = userSnapshots.docs[0].data();
+        const bytes = CryptoJS.AES.decrypt(user.password, "findvancouverdoctor");
+        const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
+
+        if (originalPassword !== payload.password) {
+            throw new Error("Incorrect Password")
+        }
+
+        return {
+            success: true,
+            message: "User logged in seccessfully",
+            data : user
+        };
+
+    } catch (error) {
+        return error;
+    }
+
+};
