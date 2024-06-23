3d5ed66d0f57eee1452780e8d7c9f8a7ee4bfcb9
Neil Quizon <neil.quizon@gmail.com>
2024-06-23 13:10:59 -0700

    create profile index

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -18,21 +18,25 @@ function ProtectedRoute({ children }) {
 
 
     return (
-        <div className="layout p-2">
+        <div className="layout p-1">
             <div className="header bg-white p-2 flex justify-between items-center">
                 <h2>
                     <strong>FINDING VANCOUVER DOCTOR</strong>
                 </h2>
                 {user && (
-                <div className="flex gap-1 items-center">
+                <div className="flex gap-3 items-center">
+                    <div className="flex gap-1 items-center">
                     <i className="ri-shield-user-line"></i>
-                    <h4 className="uppercase cursor-pointer underline">{user.name}</h4>
+                    <h4 className="uppercase cursor-pointer underline">
+                        {user.name}
+                        </h4>
+                        </div>
 
                     <i className="ri-logout-box-r-line"></i>
                 </div>
     )}
             </div>
-            <div className="content">{children}</div>
+            <div className="content my-1">{children}</div>
         </div>
     );
 }
